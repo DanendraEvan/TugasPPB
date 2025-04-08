30c7736653981e102fa30167e771c587d8935fbe
Danendra Evan Wicaksono <evanpplgb@gmail.com>
2025-04-08 22:47:37 +0700

    Delete SMART-HIDROPONIK/Source kode Smart Hidroponik/database.py

--- SMART-HIDROPONIK/Source kode Smart Hidroponik/database.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, request, jsonify # Memanggil flask untuk membuat local server , request untuk  dan jsonify
-import paho.mqtt.client as mq
-from pymongo import MongoClient as mongo
-
-web = Flask(__name__)
-
