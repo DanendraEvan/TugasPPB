92f77249ac8f63710be6da1ec17056e93633419d
DanendraEvan <evanpplgb@gmail.com>
2025-04-09 14:09:45 +0700

    Initial commit on Project 2

---
+++ PROJEK FULL/Project sekolah/PROJECT22025NAVIGASIMENU/app/src/main/java/com/example/project2_2025_navigasi_menu/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.project2_2025_navigasi_menu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnPindah;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPindah = findViewById(R.id.btnPindah);
+
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
